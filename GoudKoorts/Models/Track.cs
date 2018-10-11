e165737f47dc6b7884f1269851642dd87adba336
Marijn <mdoomens@avans.nl>
2018-10-11 13:34:21 +0200

    Initial
    Klassenstructuur gemaakt met references

---
+++ GoudKoorts/Models/Track.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoudKoorts
+{
+    public abstract class Track
+    {
+        private Movable _movable;
+
+    }
+}
---
+++ GoudKoorts/Proces/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoudKoorts
+{
+
+
+    public class Controller
+    {
+        private InputView InputView;
+        private OutputView OutputView;
+        private Map map;
+
+
+
+    }
+
+}
