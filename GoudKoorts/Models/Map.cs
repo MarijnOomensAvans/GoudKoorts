72c6ddf2da151e9e17f6b4c25e0a7b68fe4a1df4
Gijs Verwiel <gijsverwiel@hotmail.com>
2018-10-17 10:32:05 +0200

    1

--- GoudKoorts/Models/Map.cs
+++ GoudKoorts/Models/Map.cs
@@ -9,5 +9,10 @@ namespace GoudKoorts
     {
         private SpawnPoint[] spawnPoints;
 
+        public Map()
+        {
+
+        }
+
     }
 }
---
+++ GoudKoorts/Models/Switch2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoudKoorts.Models
+{   //2 to 1 switch
+    public class Switch2 : Track
+    {
+        public Track PrevTrack1 { get; set; }
+        public Track PrevTrack2 { get; set; }
+
+        public Switch2(Track prevTrack1, Track prevTrack2)
+        {
+            PrevTrack1 = prevTrack1;
+            PrevTrack2 = prevTrack2;
+        }
+
+        public override char Print()
+        {
+            return 'S';
+        }
+    }
+}
